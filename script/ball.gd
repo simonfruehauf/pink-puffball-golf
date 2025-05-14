138ad1e4efc7aa6ddfe94ad59ead66b08a4803a5
Simon <stockfisch97@gmail.com>
2025-05-14 10:48:05 +0200

    Basic ball movement

--- script/ball.gd
+++ script/ball.gd
@@ -1,60 +1,89 @@
 extends RigidBody3D
 
-## Kirby-style Golf Ball Mechanics ##
-enum ShotType { PUTT, POWER, LOB }
-@export var turn_increment: float = 15.0  # Degrees per snap turn
+enum Modes {SHORT = 0, LONG = 1, JUMP = 2}
 
-# Shooting properties
-var current_direction: Vector3 = Vector3.FORWARD
-var shot_type: ShotType = ShotType.PUTT
-var shot_presets = {
-	ShotType.PUTT: {"force": 5.0, "angle": 0.0},
-	ShotType.POWER: {"force": 10.0, "angle": 5.0},
-	ShotType.LOB: {"force": 7.0, "angle": 45.0}
-}
-@export var label : Label
-@export var label2 : Label
-var shot_power = 0.1
-var charging = false
-var charging_dir = 1.0
-func _input(event):
-	# Direction control
-	if Input.is_action_just_pressed("turn_left"):
-		current_direction = current_direction.rotated(Vector3.UP, deg_to_rad(turn_increment))
-	if Input.is_action_just_pressed("turn_right"):
-		current_direction = current_direction.rotated(Vector3.UP, deg_to_rad(-turn_increment))
-	# Shot selection
-	if Input.is_action_just_pressed("shot_next"):
-		shot_type = wrapi(shot_type + 1, 0, ShotType.size())
-	if Input.is_action_just_pressed("shot_prev"):
-		shot_type = wrapi(shot_type - 1, 0, ShotType.size())
-	if Input.is_action_just_pressed("shoot") && charging:
+@export var max_force: float = 10.0
+@export var force: float = 5.0
+var can_shoot = true
+var stop_below_velocity: float = 0.15
+var shotstart = false
+@export var step_size: float = 5.0
+var force_increment_size: float = 2.5
+@export var gravity : Vector3 = Vector3(0, 9.8, 0.0)
+
+var aim_rotation: float = 0.0
+var v: Vector3 = Vector3(1, 1, 0)
+@export var mode: Modes = Modes.SHORT
+@export var force_bar : ProgressBar
+@export var mode_label : Label
+
+func _ready() -> void:
+	force_bar.min_value = 0
+	force_bar.max_value = max_force
+	force_bar.value = force
+
+func _unhandled_input(event):
+	if event.is_action_pressed("shoot"):
 		shoot()
-		charging = false
-	elif Input.is_action_just_pressed("shoot") && !charging:
-		charging = true
+	elif event.is_action_pressed("mode_switch"):
+		mode_switch()
+	
+	# Handle aim rotation
+	if event.is_action("rotate_left"):
+		if event.is_pressed():
+			rotate_aim(-1.0)
+	
+	if event.is_action("rotate_right"):
+		if event.is_pressed():
+			rotate_aim(1.0)
+	
+	# Handle force adjustment
+	if event.is_action_pressed("increase_force"):
+		adjust_force(1.0)
+	elif event.is_action_pressed("decrease_force"):
+		adjust_force(-1.0)
 
-func _physics_process(delta: float) -> void:
-	if shot_power > 2.0:
-		charging_dir = -1.0
-		shot_power = 2.0
-	elif shot_power < 0.1:
-		charging_dir = 1.0
-		shot_power = 0.1
-	if charging:
-		shot_power += 0.02 * charging_dir
-	else:
-		shot_power = 0.1
+func mode_switch():
+	mode = (mode + 1) % 3
+	mode_label.text = Modes.keys()[mode]
+	set_v(force)
+	draw_preview()
 
-	label.text = str(shot_power)
-	label2.text = str(shot_type)
 func shoot():
-	var launch_vector = current_direction.rotated(
-		Vector3.RIGHT, 
-		deg_to_rad(shot_presets[shot_type].angle)
-		).normalized() * shot_presets[shot_type].force * shot_power
-	apply_impulse(launch_vector, Vector3.ZERO)
+	if can_shoot:
+		can_shoot = false
+		freeze = false
+		shotstart = true
+		print(v)
+		apply_central_impulse(v)
+		await get_tree().create_timer(0.25).timeout
+		shotstart = false
+
+func rotate_aim(dir):
+	aim_rotation += step_size * dir
+	draw_preview()
+	set_v(force)
+	return
+
+func adjust_force(f):
+	force += force_increment_size * f
+	force = clampf(force, 0, max_force)
+	force_bar.value = force
+	set_v(force)
+	draw_preview()
+	return
+
+func draw_preview():
 	return
 	
-func visualize():
-	return
+func _physics_process(delta):
+	if linear_velocity.length() < stop_below_velocity && !shotstart:
+		freeze = true
+		can_shoot = true
+
+func set_v(f: float):
+	var x = cos(deg_to_rad(aim_rotation)) * f
+	var z = sin(deg_to_rad(aim_rotation)) * f
+	v = Vector3(x, f if mode == Modes.JUMP else 0.0, z)
+	print(force)
+	print(v)
